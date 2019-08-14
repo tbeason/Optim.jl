9e3e17f0cba2229187447b02dd0ff9c2869ae05e
Tyler Beason <tbeas12@gmail.com>
2019-08-14 09:01:56 -0700

    -fix tests

--- src/multivariate/solvers/zeroth_order/particle_swarm.jl
+++ src/multivariate/solvers/zeroth_order/particle_swarm.jl
@@ -30,13 +30,10 @@ particle and move it away from its (potentially and probably) local optimum, to
 improve the ability to find a global optimum. Of course, this comes a the cost
 of slower convergence, but hopefully converges to the global optimum as a result.
 
-<<<<<<< HEAD
 Note, that convergence is never assessed for ParticleSwarm. It will run until it
 reaches the maximum number of iterations set in Optim.Options(iterations=x)`.
-=======
 Multi-threaded objective function evalution can be enabled by setting `threaded=true`,
 provided Julia was started with multiple threads.
->>>>>>> threading enable by flag
 
 ## References
 - [1] Zhan, Zhang, and Chung. Adaptive particle swarm optimization, IEEE Transactions on Systems, Man, and Cybernetics, Part B: CyberneticsVolume 39, Issue 6 (2009): 1362-1381
